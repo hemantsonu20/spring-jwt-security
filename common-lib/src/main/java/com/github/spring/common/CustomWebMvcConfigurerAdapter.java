36183324ab889154374250ef908d5e9be4cbc646
Hemant Patel <hemantsonu20l@gmail.com>
2017-01-18 12:43:44 +0530

    removed annotation

--- common-lib/src/main/java/com/github/spring/common/CustomWebMvcConfigurerAdapter.java
+++ common-lib/src/main/java/com/github/spring/common/CustomWebMvcConfigurerAdapter.java
@@ -12,7 +12,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  *
  */
 @Configuration
-@EnableWebSecurity
 public class CustomWebMvcConfigurerAdapter extends WebMvcConfigurerAdapter {
 
 }
