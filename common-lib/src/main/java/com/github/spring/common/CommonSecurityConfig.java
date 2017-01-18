02eaf085474e387782e7034d095b6a43d3d8cc45
Hemant Patel <hemantsonu20l@gmail.com>
2017-01-18 12:42:16 +0530

    added comment

--- common-lib/src/main/java/com/github/spring/common/CommonSecurityConfig.java
+++ common-lib/src/main/java/com/github/spring/common/CommonSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationProcessingFilter;
 
 import com.github.spring.common.filter.FilterConfiguration;
 
@@ -12,8 +13,8 @@ import com.github.spring.common.filter.FilterConfiguration;
  * 
  * <ul>
  * 
- * <li>
- *  {@link EnableResourceServer &#064;EnableResourceServer} adds a security filter 
+ * <li>{@link EnableResourceServer &#064;EnableResourceServer} adds a security
+ * filter {@link OAuth2AuthenticationProcessingFilter}
  * 
  * </li>
  * 
@@ -26,7 +27,7 @@ import com.github.spring.common.filter.FilterConfiguration;
 @Configuration
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@Import({Securityproperties.class, CustomWebMvcConfigurerAdapter.class, FilterConfiguration.class})
+@Import({ Securityproperties.class, CustomWebMvcConfigurerAdapter.class, FilterConfiguration.class })
 public class CommonSecurityConfig {
 
 }
