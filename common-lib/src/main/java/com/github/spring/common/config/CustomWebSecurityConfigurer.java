169b80d29a18c9db01a361d3adc1d4cba926679a
hemant <hemantsonu20@gmail.com>
2017-01-23 20:59:42 +0530

    added ant path matcher

--- common-lib/src/main/java/com/github/spring/common/config/CustomWebSecurityConfigurer.java
+++ common-lib/src/main/java/com/github/spring/common/config/CustomWebSecurityConfigurer.java
@@ -1,73 +1,83 @@
 package com.github.spring.common.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.OrRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
-import org.springframework.stereotype.Component;
 
-@Component
-public class CustomWebSecurityConfigurer extends WebSecurityConfigurerAdapter implements RequestMatcher, InitializingBean {
+@Configuration
+public class CustomWebSecurityConfigurer extends WebSecurityConfigurerAdapter implements InitializingBean {
 
-    @Value("${security.ignored}")
-    private List<String> ignoredPath;
-    
-    private OrRequestMatcher matcher;
+    @Value("${security.ignored:}")
+    private String[] currentIgnoredPath;
 
+    private String[] newIgnoredPath;
 
-    private List<RequestMatcher> getAntPathMatcher() {
+    private static final String SLASH = "/";
+    private static final String ALL_MATCHER = "**";
 
-        List<RequestMatcher> matchers = new ArrayList<>();
-        for (String s : ignoredPath) {
-
-            if (StringUtils.isBlank(s)) {
-                continue;
-            }
-            if (s.endsWith("/")) {
-
-                matchers.add(new AntPathRequestMatcher(StringUtils.removeEnd(s, "/")));
-            }
-            else {
-                matchers.add(new AntPathRequestMatcher(StringUtils.join(s, "/")));
-            }
-            matchers.add(new AntPathRequestMatcher(s));
-        }
-        return matchers;
-    }
-
-   
-    
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    
-        
-        
-        super.configure(http);
-        http.requestMatcher(this);
-    }
+    public void configure(WebSecurity web) throws Exception {
 
+        super.configure(web);
+        if(newIgnoredPath.length > 0) {
+            web.ignoring().antMatchers(newIgnoredPath);
+        }
+    }
 
     @Override
     public void afterPropertiesSet() throws Exception {
 
-       System.out.println(ignoredPath);
-       matcher = new OrRequestMatcher(getAntPathMatcher());
-       System.out.println(matcher);
+        newIgnoredPath = new String[currentIgnoredPath.length];
+        int i = 0;
+        for (String s : currentIgnoredPath) {
+
+            if (alternatePathNeeded(s)) {
+                newIgnoredPath[i++] = getAlternatePath(s);
+            }
+        }
+        newIgnoredPath = Arrays.copyOf(newIgnoredPath, i);
+        System.out.println(Arrays.toString(newIgnoredPath));
     }
 
-    @Override
-    public boolean matches(HttpServletRequest request) {
+    /**
+     * Method checks whether ignored path is an all ant matcher (ends with /**),
+     * in that case no need to ignore alternate path as it will be obsolete.
+     * 
+     * @param s
+     * @return
+     */
+    private boolean alternatePathNeeded(String s) {
 
-        System.out.println("called");
-        return !matcher.matches(request);
+        if (StringUtils.isBlank(s)) {
+            return false;
+        }
+        else if (s.endsWith(ALL_MATCHER)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    /**
+     * If string ends with "/" method returns removing the "/" and if it
+     * doesn't, it returns appending the "/"
+     * 
+     * @param s
+     * @return
+     */
+    private String getAlternatePath(String s) {
+
+        if (s.endsWith(SLASH)) {
+            return StringUtils.removeEnd(s, SLASH);
+        }
+        else {
+            return StringUtils.join(s, SLASH);
+        }
     }
 }
